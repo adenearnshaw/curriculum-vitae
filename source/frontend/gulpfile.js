48aaf3296c3acfac74319730dd4b7f6dc4310d63
Aden Earnshaw <aearnshaw@outlook.com>
2019-05-02 22:41:57 +0100

    Updating gulp task to merge view & data

--- source/frontend/gulpfile.js
+++ source/frontend/gulpfile.js
@@ -1,27 +1,28 @@
 const gulp = require('gulp');
 const fs = require('fs');
 
-const projectsDirectory = './src/assets/projects/data/';
+const projectViewsDirectory = './src/app/projects/data/';
+const projectDataFile = './src/assets/data/projects.json';
 
 gulp.task('clean', () => {
     return del(['./dist/**/*'], { force: true });
 });
 
 gulp.task('update-project-data', (done) => {
-    const htmlFiles = fs.readdirSync(projectsDirectory)
+    const htmlFiles = fs.readdirSync(projectViewsDirectory)
                         .filter(file => file.endsWith('.html'));
 
+    const projectDataJson = fs.readFileSync(projectDataFile, { encoding: 'utf8'});
+    const projectData = JSON.parse(projectDataJson);
+
     htmlFiles.forEach(file => {
-        const htmlContents = fs.readFileSync(`${projectsDirectory}${file}`, { encoding: 'utf8'});
+        const htmlContents = fs.readFileSync(`${projectViewsDirectory}${file}`, { encoding: 'utf8'});
 
-        const jsonFile = file.replace('.html', '.json');
-        const jsonContents = fs.readFileSync(`${projectsDirectory}${jsonFile}`, { encoding: 'utf8'});
-
-        const data = JSON.parse(jsonContents);
-        data.description = htmlContents;
-
-        fs.writeFileSync(`${projectsDirectory}${jsonFile}`, JSON.stringify(data));
+        const projectId = file.replace('.html', '');
+        projectData.find(p => p.id === projectId).description = htmlContents;
     });
+
+    fs.writeFileSync(projectDataFile, JSON.stringify(projectData, null, '    '));
 
     done();
 });
