983ff6ef08b35080b184e267e266abd791493a42
Aden Earnshaw <aearnshaw@outlook.com>
2019-05-06 07:29:53 +0100

    Adding routing to project detail - WIP

--- source/frontend/src/app/projects/projects.module.ts
+++ source/frontend/src/app/projects/projects.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { ProjectDetailComponent } from './project-detail/project-detail.component';
 import { ProjectListPreviewComponent } from './project-list-preview/project-list-preview.component';
@@ -8,6 +9,16 @@ import { ProjectListComponent } from './project-list/project-list.component';
 @NgModule({
     imports: [
         CommonModule,
+        RouterModule.forChild([
+            {
+                path: 'projects',
+                component: ProjectListComponent,
+            },
+            {
+                path: 'projects:projectId',
+                component: ProjectDetailComponent,
+            },
+        ]),
     ],
     declarations: [
         ProjectDetailComponent,
