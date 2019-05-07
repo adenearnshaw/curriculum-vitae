a4016b7f6b393c1178a29d433a6b33406c8c145d
Aden Earnshaw <aearnshaw@outlook.com>
2019-05-07 17:49:34 +0100

    Updating Shortcuts description & Employment skills

--- source/frontend/src/app/projects/project-detail/project-detail.component.ts
+++ source/frontend/src/app/projects/project-detail/project-detail.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { map, first, filter } from 'rxjs/operators';
 
 import { DataService } from '../../core/data.service';
 import { IProjectItem } from '../../core/models/project-item';
@@ -8,7 +7,8 @@ import { IProjectItem } from '../../core/models/project-item';
 @Component({
     selector: 'cv-project-detail',
     templateUrl: './project-detail.component.html',
-    styleUrls: ['./project-detail.component.scss']
+    styleUrls: ['./project-detail.component.scss'],
+    encapsulation: ViewEncapsulation.None,
 })
 export class ProjectDetailComponent implements OnInit {
     public project: IProjectItem;
