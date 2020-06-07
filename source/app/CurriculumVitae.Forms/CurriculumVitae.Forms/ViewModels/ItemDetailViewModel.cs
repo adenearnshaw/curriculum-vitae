776fa54d0ea16f154e4c464bf716eb81c7327488
Aden Earnshaw <aearnshaw@outlook.com>
2020-06-07 16:21:58 +0100

    Adding Shell Template

---
+++ source/app/CurriculumVitae.Forms/CurriculumVitae.Forms/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using CurriculumVitae.Forms.Models;
+
+namespace CurriculumVitae.Forms.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
